a8ddb6cdb6ab0b45571f476a80a5d23ead02b995
Anush Serrao <60592571+ashserrao@users.noreply.github.com>
2024-07-12 12:24:14 +0530

    deleted unwanted file

--- src/Systemcheck/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import Systemcheck from "./systemcheck";
-
-render(<Systemcheck />, window.document.querySelector("#app-container"));
-
-if (module.hot) module.hot.accept();
